b80d4fd2d673815eb5c10b70e52eb357c57df848
KalvinKouch <kskouch@uci.edu>
2024-06-02 18:25:57 -0700

    Connected data to google maps

--- PeakPulse/src/Components/GoogleMaps.jsx
+++ PeakPulse/src/Components/GoogleMaps.jsx
@@ -7,16 +7,30 @@ import {
     AdvancedMarker,
     Pin,
 } from "@vis.gl/react-google-maps"
-
+import { useNavigate } from 'react-router-dom';
+import data from "./data.json";
 function GoogleMaps() {
+
+  const navigate = useNavigate();
+
+  const handleSuggestionClick = (suggestion) => {
+    console.log("Clicked suggestion:", suggestion);
+    localStorage.setItem('selectedName', suggestion.name);
+    localStorage.setItem('selectedData', JSON.stringify(suggestion));
+    navigate('/statistics');
+}
+
     let position = { lat: 33.6846, lng: -117.8265}
   return (
     <APIProvider apiKey={APIKEY()}>
         <div style={{height: "500px", width: "500px"}}>
             <Map defaultZoom={14} defaultCenter={position} mapId={MAPID()}>
-              <AdvancedMarker position={position}>
-                <Pin background={"blue"}/>
-              </AdvancedMarker>
+            {data.map((suggestion, index) => {
+              console.log(suggestion.coordinates);
+              return (<AdvancedMarker key={index} position={suggestion.coordinates} onClick={() => handleSuggestionClick(suggestion)}>
+                <Pin background={"grey"} glyphColor={"white"} borderColor={"grey"}/>
+              </AdvancedMarker>)
+            })}
             </Map>
         </div>
     </APIProvider>
